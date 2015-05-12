cbe8ce1bd3c4bc5af2151dd5895591f4e913faa4
Kurt Kartaltepe <kkartalepe@gmail.com>
2015-05-11 22:02:45 -0500

    Compiles in cargo

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#![crate_type = "lib"]
+#![feature(libc)]
+#![allow(unused_imports, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+
+
+extern crate libc;
+mod odeconfig;
+mod common;
+mod odeinit;
+mod contact;
+mod error;
+mod memory;
+mod odemath;
+mod matrix;
+mod timer;
+mod rotation;
+mod mass;
+mod misc;
+mod objects;
+mod collision_space;
+mod collision;
+mod threading;
+mod export_dif;
+
+//pub use odeconfig::*;
+//pub use error::*;
+pub use common::*;
+pub use odeinit::*;
+pub use contact::*;
+pub use memory::*;
+pub use odemath::*;
+pub use matrix::*;
+pub use timer::*;
+pub use rotation::*;
+pub use mass::*;
+pub use misc::*;
+pub use objects::*;
+pub use collision_space::*;
+pub use collision::*;
+pub use threading::*;
+pub use export_dif::*;
